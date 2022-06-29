e57e537de5ea7d6e09fd0f088474539fde287117
fsampson30 <fsampson30@gmail.com>
2022-06-29 07:26:18 -0300

    Changed class Success to data class.

--- app/src/main/java/com/techyourchance/dagger2course/Constants.kt
+++ app/src/main/java/com/techyourchance/dagger2course/Constants.kt
@@ -3,5 +3,4 @@ package com.techyourchance.dagger2course
 object Constants {
     const val BASE_URL = "https://api.stackexchange.com/2.2/"
     const val STACKOVERFLOW_API_KEY = "ZiXCZbWaOwnDgpVT9Hx8IA(("
-    const val DEV_NAME = "Flavio Sampson"
 }
